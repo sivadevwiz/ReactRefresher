472b3f8cd963e83bb11ca9367d3878bca567b31d
SivaDevWiz <siva.devwiz@gmail.com>
2023-03-30 23:56:19 +0530

    adding TicTacToe file

--- react_vite/src/App.tsx
+++ react_vite/src/App.tsx
@@ -4,6 +4,7 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 import UseStateHook from "./hooks/UseStateHook";
+import TicTacToe from "./tic-tac-toe/TicTacToe";
 
 function App() {
   // const [count, setCount] = useState(0);
@@ -11,7 +12,8 @@ function App() {
   return (
     <>
       <div className="App"></div>
-      <UseStateHook></UseStateHook>
+      {/* <UseStateHook></UseStateHook> */}
+      <TicTacToe></TicTacToe>
     </>
   );
 }
